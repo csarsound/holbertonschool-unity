ab29ea3ca073a1f3f009d11cd48d3c73c108a9db
Cesar Daniel Martinez Sandino <csar@MacBook-Pro-de-Cesar.local>
2023-07-16 20:32:36 -0500

    FallingImpactAudio

--- unity-audio/Assets/Scripts/AudioPlayer.cs
+++ unity-audio/Assets/Scripts/AudioPlayer.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
-using UnityEngine.Audio;
 
-public class AudioPlayer : MonoBehaviour
+public class FootstepController : MonoBehaviour
 {
     public AudioClip footstepsRunningGrass;
     public AudioClip footstepsRunningRock;
-    public AudioClip landingGrass;
-    public AudioClip landingRock;
 
     private AudioSource audioSource;
-    public AudioMixerGroup landingAudioGroup;
-    public AudioMixerGroup runningAudioGroup;
     public Animator animator;
 
     void Start()
@@ -20,36 +15,8 @@ public class AudioPlayer : MonoBehaviour
 
     void Update()
     {
-        bool landingActive = animator.GetCurrentAnimatorStateInfo(0).IsTag("FallingImpact");
-
-        if (landingActive)
-        {
-            audioSource.outputAudioMixerGroup = landingAudioGroup;
-            // Obtener el tipo de suelo debajo del jugador
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f))
-            {
-                // Reproducir el sonido de los pasos según el tipo de suelo
-                if (hit.collider.CompareTag("Grass"))
-                {
-                    audioSource.clip = landingGrass;
-                }
-                else if (hit.collider.CompareTag("Rock"))
-                {
-                    audioSource.clip = landingRock;
-                }
-
-                // Reproducir el sonido de los pasos en bucle
-                if (!audioSource.isPlaying)
-                {
-                    audioSource.Play();
-                    Debug.Log("AudioClip");
-                }
-            }
-        }
         if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Running"))
         {
-            audioSource.outputAudioMixerGroup = runningAudioGroup;
             // Obtener el tipo de suelo debajo del jugador
             RaycastHit hit;
             if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f))
